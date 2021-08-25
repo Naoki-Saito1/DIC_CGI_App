61cd03c39738d6fd2b7e4a086ee0b15279b0fdc9
saito naoki <reisa.naoki@icloud.com>
2021-08-25 14:12:31 +0900

    課題再提出

--- test.rb
+++ test.rb
@@ -1,12 +1,13 @@
 require 'webrick'
 server = WEBrick::HTTPServer.new({
-  :DocumentRoot => '.',
+  :DocumentRoot => '/',
   :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
   :Port => '3000',
 })
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
+server.mount('/',WEBrick::HTTPServlet::ERBHandler, 'kadai2.html.erb')
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.start
